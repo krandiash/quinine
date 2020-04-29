4240308bf29b0539735a6d9f9b134024188742cf
krandiash <kgoel93@gmail.com>
2020-04-28 19:58:04 -0700

    debug

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .quinine import *
--- quinine/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .quinfig import Quinfig
-#
-# Register all schemas
-# create_and_register_schemas()
-#
-# __all__ = ["quinfig"]
-# from .quinfig import Quinfig
