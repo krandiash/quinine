67b733297bd26709f5283784f79f4f86c83f032b
krandiash <kgoel93@gmail.com>
2020-05-08 04:13:26 -0700

    added QuinineArgumentParser

--- quinine/__init__.py
+++ quinine/__init__.py
@@ -1,4 +1,5 @@
 from .quinfig import Quinfig
 from .common.sweep import QuinSweep
+from .common.argparse import QuinineArgumentParser
 from .common.cerberus import tstring, tinteger, tfloat, tboolean, tlist, tdict, required, nullable, \
     default, excludes, schema, allowed, stlist, stdict
